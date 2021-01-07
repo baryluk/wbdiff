26e2d5925c1372f17a2facc97d69c617298c3c3d
Witold Baryluk <witold.baryluk@gmail.com>
2021-01-07 09:00:30 +0000

    Update to Python3. Fix commants and binary operators.
    Use `print()`.
    Use `range` instead of `xrange`.
    Use `list(range(len))`, instead of `range(len)` where needed.
    Use `enumerate` instead of custom `zip(iter.count(), seq)`
    Make comments nicer to read (Capitalize, wrap long lines).
    Make almost all binary operators have space around them.

--- wbdiff.py
+++ wbdiff.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # wbdiff.py - sophisticated diff utility
 #
-# Copyright: Witold Baryluk, 2013
+# Copyright: Witold Baryluk, 2013, 2021
 #
 # MIT license
 #
@@ -126,12 +126,6 @@ replacments = {
   "Log timestamp": (r"^(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [012][0-9] [012][0-9]:[0-6][0-9]:[0-6][0-9] (\w+(?:\.\w+)*) kernel: \[ *[0-9]+\.[0-9]+\] ", r"\1  kernel:  ")
 }
 
-
-import itertools
-
-def iterseq(sequence):
-  return zip(itertools.count(), sequence)
-
 # http://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python
 class _ansi:
   HEADER = '\033[95m'
@@ -178,8 +172,8 @@ def similar(a, b, threshold):
 leftfilename = args.files[0]
 rightfilename = args.files[1]
 
-file_reader1 = file
-file_reader2 = file
+file_reader1 = lambda filename: open(filename, "r")
+file_reader2 = lambda filename: open(filename, "r")
 if args.read_compressed_files:
   if leftfilename.endswith(".gz"):
     import gzip
@@ -211,7 +205,7 @@ def apply_replacements_line(compiled_replacments, line):
   return line
 
 def apply_replacements(compiled_replacments, org):
-  for i, line in iterseq(org):
+  for i, line in enumerate(org):
     org[i] = apply_replacements_line(compiled_replacments, line)
   return org
 
@@ -220,45 +214,46 @@ if args.use_default_replacments:
   left = apply_replacements(compiled_replacments, left)
   right = apply_replacements(compiled_replacments, right)
 
-# compute similarity matrix
+# Compute similarity matrix.
 leftlen = len(left)
 rightlen = len(right)
-m = range(leftlen)
-for i, a in iterseq(left):
-  m[i] = range(rightlen)
-  for j, b in iterseq(right):
+m = list(range(leftlen))
+for i, a in enumerate(left):
+  m[i] = list(range(rightlen))
+  for j, b in enumerate(right):
     #m[i][j] = line_similarity(a, b)
     if a == b:
       m[i][j] = 1.0
     else:
       m[i][j] = 0.0
 
-# use context for similarity measure
-m2 = range(leftlen)
-for i, a in iterseq(left):
-  m2[i] = range(rightlen)
-  for j, b in iterseq(right):
+# Use context for similarity measure.
+m2 = list(range(leftlen))
+for i, a in enumerate(left):
+  m2[i] = list(range(rightlen))
+  for j, b in enumerate(right):
     s = 1.0
     kk = 1
-    # extend using context, but do no go beyond context_matching limit or last lines
-    for k in xrange(args.initial_context_matching):
-      if i+k<leftlen and j+k<rightlen:
+    # Extend using context, but do no go beyond `context_matching` limit
+    # or last lines.
+    for k in range(args.initial_context_matching):
+      if i + k < leftlen and j + k < rightlen:
         kk += 1
-        if m[i+k][j+k] == 0.0:
+        if m[i + k][j + k] == 0.0:
           s = 0.0
           break
-        #s *= m[i+k][j+k]
-    #s = s ** (1.0/kk)
+        #s *= m[i + k][j + k]
+    #s = s ** (1.0 / kk)
 # some tweeking to weights
 #    if s < 0.90:
 #      s = 0.0
 #    else:
-#      s = (s-0.90)*10.0
-#      s = 1.0 - (1.0-s)**2
+#      s = (s - 0.90) * 10.0
+#      s = 1.0 - (1.0 - s)**2
     m2[i][j] = s
 
-matched_left = [False]*leftlen
-matched_right = [False]*rightlen
+matched_left = [False] * leftlen
+matched_right = [False] * rightlen
 
 
 LL = ""
@@ -271,20 +266,20 @@ if args.line_prefixes:
 
 blocks_right = {}
 
-# search for long common subsequences, and extend them.
-for j, _b in iterseq(right):
+# Search for long common subsequences, and extend them.
+for j, _b in enumerate(right):
   if matched_right[j]:
     continue
-  for i, _a in iterseq(left):
+  for i, _a in enumerate(left):
     if matched_left[i]:
       continue
     if m2[i][j] > 0.95:
       matched_left[i] = True
       matched_right[j] = True
       k = 0
-      while i+k < leftlen and j+k < rightlen and m[i+k][j+k] > 0.95: # m not m2 !
-        matched_left[i+k] = True
-        matched_right[j+k] = True
+      while i + k < leftlen and j + k < rightlen and m[i + k][j + k] > 0.95: # m not m2 !
+        matched_left[i + k] = True
+        matched_right[j + k] = True
         k += 1
       blocks_right[j] = (i, k)
 
@@ -299,84 +294,85 @@ def red(s):
 def green(s):
   return ansi.OKGREEN+s+ansi.ENDC
 
-W=2
+W = 2
 if leftlen > 99 or rightlen > 99:
   W = 3
 if leftlen > 999 or rightlen > 999:
   W = 4
-W += 1 # workaround around IMHO % 3d bug, TODO(baryluk): fix it
+W += 1 # workaround around IMHO % 3d bug, TODO(baryluk): Fix it.
 
-WW="%s% "+str(W)+"d"
-WL=LL+"% "+str(W)+"d"
-WR=RR+"% "+str(W)+"d"
-XX=EE+" "*W
+WW = "%s% " + str(W) + "d"
+WL = LL + "% " + str(W) + "d"
+WR = RR + "% " + str(W) + "d"
+XX = EE + " "*W
 
-# Print common subsequences, intermixing new lines (in natural order), and removed ones (just after end of moved ones)
-print red("Removals")+" from left file, "+blue("moves")+" and "+green("additions")+" to right file:"
-print red("--- "+leftfilename)
-print green("+++ "+rightfilename)
+# Print common subsequences, intermixing new lines (in natural order),
+# and removed ones (just after end of moved ones).
+print(red("Removals") + " from left file, " + blue("moves") + " and " + green("additions") + " to right file:")
+print(red("--- " + leftfilename))
+print(green("+++ " + rightfilename))
 
-# first print lines removed from left file, before any blocks in right file
-for i, a in iterseq(left):
+# First print lines removed from left file, before any blocks in right file.
+for i, a in enumerate(left):
   if not matched_left[i]:
     left_removed += 1
-    print (WL+"->"+XX+" "+red("- %s")) % (i+1, a.rstrip())
+    print((WL + "->" + XX + " " + red("- %s")) % (i + 1, a.rstrip()))
   else:
     break
 
-# right centric view
+# Right centric view.
 
-for j, _b in iterseq(right):
+for j, _b in enumerate(right):
   if j in blocks_right:
     i, k = blocks_right[j]
-    print "-"*(2*W+3) + "\\ l%04d-l%04d -> r%04d-r%04d  (%d lines)" % (i+1, i+k+1, j+1, j+k+1, k)
+    print("-"*(2 * W + 3) + "\\ l%04d-l%04d -> r%04d-r%04d  (%d lines)" % (i + 1, i + k + 1, j + 1, j + k + 1, k))
     was_shortened = False
-    for kk in xrange(k):
+    for kk in range(k):
       left_right_matched += 1
       c = "|"
-      if kk < args.shorten_long_matches/2 or kk>(k-args.shorten_long_matches/2):
-        print (WL+"->"+WR+" "+blue("%s %s")) % (i+kk+1, j+kk+1, c, left[i+kk].rstrip())
+      if kk < args.shorten_long_matches / 2 or kk > (k - args.shorten_long_matches / 2):
+        print((WL + "->" + WR + " " + blue("%s %s")) % (i + kk + 1, j + kk + 1, c, left[i + kk].rstrip()))
       else:
         if was_shortened:
           continue
-        print " "+"."*(W-1)+"->"+" "+"."*(W-1)+" "+blue(":")
-        print " (%d lines skiped)" % (k-args.shorten_long_matches+1,)
-        print " "+"."*(W-1)+"->"+" "+"."*(W-1)+" "+blue(":")
+        print(" " + "."*(W - 1) + "->" + " " + "."*(W - 1) + " " + blue(":"))
+        print(" (%d lines skiped)" % (k-args.shorten_long_matches + 1,))
+        print(" " + "."*(W - 1) + "->" + " " + "."*(W - 1) + " " + blue(":"))
         was_shortened = True
-    print "-"*(2*W+3) + "/"
-    i = i+k
+    print("-"*(2 * W + 3) + "/")
+    i = i + k
     while i < leftlen and not matched_left[i]:
       left_removed += 1
-      print (WL+"->"+XX+" "+red("- %s")) % (i+1, left[i].rstrip())
+      print((WL + "->" + XX + " " + red("- %s")) % (i + 1, left[i].rstrip()))
       i += 1
   if not matched_right[j]:
     right_added += 1
-    print (XX+"->"+WR+" "+green("+ %s")) % (j+1, _b.rstrip())
+    print((XX + "->" + WR + " " + green("+ %s")) % (j + 1, _b.rstrip()))
 
 if args.summary_left:
-  print red("Removed")+" from left file:"
-  for i, _a in iterseq(left):
+  print(red("Removed") + " from left file:")
+  for i, _a in enumerate(left):
     if not matched_left[i]:
-      if i > 0 and matched_left[i-1]:
-        print "-----"
-      print (WL+"->"+XX+" "+red("- %s")) % (i+1, _a.rstrip())
+      if i > 0 and matched_left[i - 1]:
+        print("-----")
+      print((WL + "->" + XX + " " + red("- %s")) % (i + 1, _a.rstrip()))
 
 if args.summary_right:
-  print green("Added")+" to right file"
-  for j, _b in iterseq(right):
+  print(green("Added") + " to right file")
+  for j, _b in enumerate(right):
     if not matched_right[j]:
-      if i > 0 and matched_right[j-1]:
-        print "-----"
-      print (XX+"->"+WR+" "+green("+ %s")) % (j+1, _b.rstrip())
+      if i > 0 and matched_right[j - 1]:
+        print("-----")
+      print((XX + "->" + WR + " " + green("+ %s")) % (j + 1, _b.rstrip()))
 
 if args.summary_stats:
-  print
-  print ansi.WARNING+"Diff statistics:"
-  print "\tInitial context length used:", args.initial_context_matching
-  print "\tNumber of matching (extended) blocks:", len(blocks_right)
+  print()
+  print(ansi.WARNING + "Diff statistics:")
+  print("\tInitial context length used:", args.initial_context_matching)
+  print("\tNumber of matching (extended) blocks:", len(blocks_right))
   # sum(map(lambda x: x[1], blocks_right.itervalues()))
-  print "\tNumber of lines in matching (extended) blocks:", left_right_matched
-  print "\tNumber of lines removed from left file:", left_removed
-  print "\tNumber of new lines added to right file:", right_added
-  print ("\tWeighted cost (blocks + removed + added): %s"+ansi.ENDC) % (len(blocks_right)-1 + left_removed + right_added,)
+  print("\tNumber of lines in matching (extended) blocks:", left_right_matched)
+  print("\tNumber of lines removed from left file:", left_removed)
+  print("\tNumber of new lines added to right file:", right_added)
+  print(("\tWeighted cost (blocks + removed + added): %s" + ansi.ENDC) % (len(blocks_right) - 1 + left_removed + right_added,))
 
